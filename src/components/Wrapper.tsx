3bc2be7b2d5357c4c71fe87303cafcb162403f76
Adriano F. Rosso <rosso.af@gmail.com>
2021-03-27 17:54:50 -0300

    next.js with chakra basic form

---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+interface WrapperProps {
+  variant?: "small" | "regular";
+}
+
+export const Wrapper: React.FC<WrapperProps> = ({
+  children,
+  variant = "regular",
+}) => {
+  return (
+    <Box m={8} w="100%" maxW={variant === "regular" ? "800px" : "400px"}>
+      {children}
+    </Box>
+  );
+};
