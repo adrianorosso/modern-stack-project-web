cc4e9c2aea050c3557af2bbceab35d8f58919c17
Adriano F. Rosso <rosso.af@gmail.com>
2021-04-25 16:12:13 -0300

    improve navbar ui

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,13 @@
-import { Box, Button, Flex, Heading, Link, LinkBox } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  LinkBox,
+  IconProps,
+  Icon,
+} from "@chakra-ui/react";
 import React from "react";
 import NextLink from "next/link";
 import { useLogoutMutation, useMeQuery } from "../generated/graphql";
@@ -18,10 +27,14 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
     body = (
       <>
         <NextLink href="/login">
-          <Link mr={2}>login</Link>
+          <Button variant="link" mr={2} fontWeight="bold" textColor="gray.900">
+            Login
+          </Button>
         </NextLink>
         <NextLink href="/register">
-          <Link mr={2}>register</Link>
+          <Button as={Link} mr={2}>
+            Register
+          </Button>
         </NextLink>
       </>
     );
@@ -31,31 +44,33 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
       <Flex align="center">
         <LinkBox ml="auto" mr={4}>
           <NextLink href="/create-post">
-            <Button as={Link}>create post</Button>
+            <Button as={Link}>Create Post</Button>
           </NextLink>
         </LinkBox>
-        <Box mr={2}>{data.me.username}</Box>
         <Button
           variant="link"
+          fontWeight="bold"
+          textColor="gray.900"
           onClick={async () => {
             await logout();
             router.reload();
           }}
           isLoading={logoutFetching}
         >
-          logout
+          Logout ({data.me.username})
         </Button>
       </Flex>
     );
   }
   return (
-    <Flex position="sticky" top={0} zIndex={1} bgColor="tomato" p={4}>
+    <Flex position="sticky" top={0} zIndex={1} bgColor="#7eb5b0" p={4}>
       <Flex flex={1} maxW={800} m="auto" align="center">
         <NextLink href="/">
           <Link>
             <Heading>Posts</Heading>
           </Link>
         </NextLink>
+
         <Box ml="auto">{body}</Box>
       </Flex>
     </Flex>
